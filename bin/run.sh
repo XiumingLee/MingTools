8fbf69de259fbfe73088395807f881b6ea16fd9c
Xiuming Lee <lxmcoder@gmail.com>
2020-06-09 12:15:53 +0800

    打包配置

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,9 @@
-export BASE_DIR=$(dirname $0)
+APP_NAME="ming-tools"
+BASE_DIR=$(dirname $0)
+APP_JAR="${BASE_DIR}/lib/ming-tools.jar"
+
 JAVA_OPT=""
 SPRING_BOOT_OPT="--spring.config.location=${BASE_DIR}/conf/application.yaml"
-APP="${BASE_DIR}/lib/ming-tools.jar"
-nohub java -jar ${JAVA_OPT} ${APP} ${SPRING_BOOT_OPT} &
+
+nohup java -jar ${JAVA_OPT} ${APP_JAR} ${SPRING_BOOT_OPT} > ${APP_NAME}.log &
+exit 1
