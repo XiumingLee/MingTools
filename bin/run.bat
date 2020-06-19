44038ffaafc9b09e66a038cc28061774788b021b
Xiuming Lee <lxmcoder@gmail.com>
2020-06-19 14:53:02 +0800

    添加Windows启动脚本

---
+++ bin/run.bat
@@ -0,0 +1,14 @@
+@echo off
+rem use gbk
+rem %APP_NAME%_start.out
+
+set BASE_DIR=%~dp0
+cd %BASE_DIR%
+set APP_NAME="ming-tools"
+set APP_JAR="%BASE_DIR%\lib\ming-tools.jar"
+set "JAVA_OPT=%JAVA_OPT% -server -Xms30m -Xmx30m -Xmn20m -XX:MetaspaceSize=50m -XX:MaxMetaspaceSize=50m"
+set SPRING_BOOT_OPT="--spring.config.location=%BASE_DIR%\conf\application.yaml"
+java -jar  %APP_JAR% %SPRING_BOOT_OPT%
+
+exit
+pause
