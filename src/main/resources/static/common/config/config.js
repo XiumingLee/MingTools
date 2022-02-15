30fd14f5e6d3e34e75c483d1a25cc2fc0d92a650
Xiuming Lee <lxmcoder@163.com>
2022-02-15 16:36:14 +0800

    update...

--- src/main/resources/static/common/config/config.js
+++ src/main/resources/static/common/config/config.js
@@ -6,6 +6,6 @@ export default {
     /** 请求超时时间，毫秒（默认2分钟） */
     request_timeout: 1200000,
     /** 请求的baseUrl */
-    request_baseUrl: '/api',
+    request_baseUrl: '/',
 
 }
--- src/main/resources/static/module/login.js
+++ src/main/resources/static/module/login.js
@@ -1,10 +1,23 @@
+import {get} from "../common/request/request.js";
+
 export default {
-  name: 'login',
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+    name: 'login',
+    data: function () {
+        return {
+            message: 'Hello Vue!'
+        }
+    },
+    mounted() {
+        console.log("我是mounted函数")
+    },
+    methods: {
+        test() {
+            console.log("我是test方法")
+            get("/test/hello").then(res => {
+                console.log(res)
+            })
+        }
+    },
+    template: `<button v-on:click="test"> {{ message }} </button>`
 }
 
